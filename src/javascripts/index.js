a9e2c57c851974a2aa2abddc1487b40239adcb43
taku22 <takutakt12@gmail.com>
2020-07-21 20:43:34 +0900

    make main content in src/archive.pug

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,7 @@
 import '../stylesheets/main.scss';
+import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
+import {faTwitter,faLine,faInstagramSquare,faFacebookSquare,faPinterestSquare} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter,faLine,faInstagramSquare,faFacebookSquare,faPinterestSquare);
+
+dom.watch();
