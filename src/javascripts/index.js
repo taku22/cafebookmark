f89de9e95c2e345e4263720a0cc6a55494577289
taku22 <takutakt12@gmail.com>
2020-07-25 16:09:34 +0900

    make hamburgermenu and fix css about related content and latestArchive

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,6 +2,9 @@ import '../stylesheets/main.scss';
 import {config, dom, library} from '@fortawesome/fontawesome-svg-core';
 import {faTwitter,faLine,faInstagramSquare,faFacebookSquare,faPinterestSquare} from '@fortawesome/free-brands-svg-icons';
 
+import hamburgerMenu  from './hamburgerMenu.js';
+
 library.add(faTwitter,faLine,faInstagramSquare,faFacebookSquare,faPinterestSquare);
 
 dom.watch();
+hamburgerMenu();
